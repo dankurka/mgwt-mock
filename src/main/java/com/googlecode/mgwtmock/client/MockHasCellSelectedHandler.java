7c9d7c870d5a341f0b6b564214f6a966ef03994e
Christian Kütbach <ckuetbach@googlemail.com>
2012-10-22 12:45:24 +0200

    Renamed file, because of typo

--- src/main/java/com/googlecode/mgwtmock/client/MockHasCellSelectedHandler.java
+++ src/main/java/com/googlecode/mgwtmock/client/MockHasCellSelectedHandler.java
@@ -9,7 +9,7 @@ import com.googlecode.mgwt.ui.client.widget.celllist.*;
  * @author ckuetbach@googlemail.com
  * 
  */
-public class MockCellSelectedHandler extends MockHasHandlers implements HasCellSelectedHandler {
+public class MockHasCellSelectedHandler extends MockHasHandlers implements HasCellSelectedHandler {
 
 	/* (non-Javadoc)
 	 * @see com.googlecode.mgwt.ui.client.widget.celllist.HasCellSelectedHandler#addCellSelectedHandler(com.googlecode.mgwt.ui.client.widget.celllist.CellSelectedHandler)
