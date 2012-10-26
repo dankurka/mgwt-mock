ccd80cae1f347667946aec9041afeda2e8c1e1c2
Christian Kütbach <ckuetbach@googlemail.com>
2012-10-26 17:45:19 +0200

    Removed wrong legal header (again) should change my filetemplate.

--- src/main/java/com/googlecode/mgwtmock/client/MockHasTouchHandlers.java
+++ src/main/java/com/googlecode/mgwtmock/client/MockHasTouchHandlers.java
@@ -1,28 +1,4 @@
-/*
- * Copyright (c) by d.velop AG. All Rights Reserved.
- * 
- * Diese Datei ist eine vertrauliche und geschützte Information der d.velop AG.
- * Das Veröffentlichen und Nutzen dieser Informationen darf nur in Übereinstimmung
- * mit den Lizenzbestimmungen der d.velop AG geschehen.
- * 
- * This file is confidential and protected information of the d.velop AG. 
- * Publishing and use of this information may happen only according to the 
- * licence regulations of the d.velop AG.
- *
- * @(#)date of creation: 25.10.2012
- * @(#)time of creation: 09:51:16
- * @(#)user of creation: ckue
- * 
- * @(#)Subversion Infos
- *
- * $Id$
- * $HeadURL$
- * $LastChangedBy$
- * $LastChangedRevision$
- * $LastChangedDate$
- * $Author$
- *
- */
+
 package com.googlecode.mgwtmock.client;
 
 import com.google.gwt.event.shared.HandlerRegistration;
